bb87a87c7989905bc617a7a3573a63a7a6884c22
MSI <criss21.dj@gmail.com>
2025-12-29 13:01:13 +0330

    most simple  config

--- phaser_e001/src/main.js
+++ phaser_e001/src/main.js
@@ -1,3 +1,28 @@
 import Phaser from "Phaser"
 
 console.log(Phaser);
+most_simple_phaser_scene_config();
+
+function most_simple_phaser_scene_config() {
+    var config = {
+        type: Phaser.AUTO,
+        width: 800,
+        height: 600,
+        scene: {
+            preload: preload,
+            create: create,
+            update: update
+        }
+    };
+
+    var game = new Phaser.Game(config);
+
+    function preload() {
+    }
+
+    function create() {
+    }
+
+    function update() {
+    }
+}
