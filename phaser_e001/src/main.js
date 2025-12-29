52be856114aef73662194441305db49282b0175f
MSI <criss21.dj@gmail.com>
2025-12-29 14:28:22 +0330

    add text sample

--- phaser_e001/src/main.js
+++ phaser_e001/src/main.js
@@ -1,7 +1,37 @@
 import Phaser from "Phaser"
 
 console.log(Phaser);
-most_simple_phaser_scene_config();
+//most_simple_phaser_scene_config();
+add_text_scene();
+
+function add_text_scene(){
+    var config = {
+        type: Phaser.AUTO,
+        width: 800,
+        height: 600,
+        backgroundColor: '#DEDEDE',
+        scene: {
+            preload: preload,
+            create: create,
+            update: update
+        }
+    };
+
+    var game = new Phaser.Game(config);
+
+    function preload() {
+    }
+
+    function create() {
+        this.add.text(250,300,'GAME TEXT',{
+            font:'32px Arial',
+            fill:'#0f0',
+        });
+    }
+
+    function update() {
+    }
+}
 
 function most_simple_phaser_scene_config() {
     var config = {
